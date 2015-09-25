c694e85483249c9bf521d39f151797eb4c9787b0
Prasanth Louis <prasanthlouis21@gmail.com>
2015-09-25 10:54:14 -0400

    Directory Commit

--- Bolt/js/controllers/MainController.js
+++ Bolt/js/controllers/MainController.js
@@ -5,7 +5,30 @@ app.controller('MainController', ['$scope', function($scope) {
   season: 3,
   episode: "The Empty Hearse",
   description: "Two years after his reported Reichenbach Fall demise, Sherlock, who has been cleared of all fraud charges against him, returns with Mycroft's help to a London under threat of terrorist attack. John has moved on and has a girlfriend, Mary Morstan. Sherlock enlists Molly to assist him, but when John is kidnapped by unknown assailants and is rescued by Sherlock and Mary, John returns to help find the terrorists and an underground plot to blow up the Houses of Parliament during an all night sitting on Guy Fawkes Night.",
-  datetime: new Date(2014, 11, 31, 21, 00, 00, 00)
-}]
+  datetime: new Date(2014, 11, 29 , 21, 00, 00, 00)
+},
+                  {series: "Big Bang Theory",
+  series_img: "http://www.audioeverywhere.com/wp-content/uploads/The-Big-Bang-Theory-Starts-Scholarship-For-UCLA-Students1.jpg",
+  genre: "Comedy",
+  season: 2,
+  episode: "The Terminator Decoupling",
+  description: "Leonard (Johnny Galecki) Sheldon (Jim Parsons) Howard (Simon Helberg) and Raj (Kunal Nayyar) are heading to San Francisco to attend a conference where cosmologist George Smoot is giving a keynote address. On Sheldons insistence they travel on the Coast Starlight train. On board the train, they learn that Summer Glau (played by herself) is riding the same train. Raj and Howard debate on who should approach her first",
+  datetime: new Date(2014, 11, 30, 22, 00, 00, 00)
+},
+                                   {series: "Mad-Men",
+  series_img: "http://untappedcities.com/wp-content/uploads/2015/03/mad-men-1024x768.jpg",
+  genre: "Drama",
+  season: 1,
+  episode: "Long Weekend",
+  description: "Betty's father Gene comes to visit for Labor Day. Since the recent death of Betty's mother, Gene has found a new companion named Gloria, to whom Betty takes an immediate dislike. The two leave for their Labor Day weekend trip, with Don promising to join them the following day. At Sterling Cooper, Don and the other ad men discuss the Richard Nixon campaign and meet with Rachel and her father about their plan to remodel her department store. Roger tries to convince Joan to spend the evening with him, but she rejects him in favor of going out on the town with her roommate.",
+  datetime: new Date(2014, 11, 31, 23, 00, 00, 00)},
+                                                     {series: "Castle",
+  series_img: "http://www.therawadvantage.com/wp-content/uploads/2013/09/Castle-Season-3.jpg",
+  genre: "Drama",
+  season: 7,
+  episode: "Dead from New York",
+  description: "The creator of the hit sketch comedy series Saturday Night Tonight is strangled with his own tie and thrown down an elevator shaft. Most of the staff was in the studio the night of the murder, including Mickey Franks (Jaleel White), a volatile star on the verge of being fired. The show must go on, with singer Carly Rae Jepsen making an appearance. Beckett ultimately must arrest the killer on live television.",
+  datetime: new Date(2015, 0, 1, 18, 00, 00, 00)}
+           ]
   
 }]);
